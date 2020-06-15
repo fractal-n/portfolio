41df01cd50a76fd43c325bc20f7c84341dc0d1df
Hien Nguyen <hien_d_nguyen@outlook.com>
2020-06-15 00:01:10 -0600

    show tags and favorite in project card

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,12 +3,7 @@
     <div class="py-12"></div>
 
     <v-container>
-      <h2
-        id="projects"
-        class="display-2 font-weight-bold mb-3 text-uppercase text-center"
-      >
-        Projects
-      </h2>
+      <h2 id="projects" class="display-2 font-weight-bold mb-3 text-uppercase text-center">Projects</h2>
 
       <v-responsive class="mx-auto mb-12" width="56">
         <v-divider class="mb-1"></v-divider>
@@ -16,29 +11,49 @@
         <v-divider></v-divider>
       </v-responsive>
 
+      <!-- <v-row align="center" justify="center">
+        <h2>Filters</h2>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-chip-group v-model="neighborhoods" column multiple>
+          <v-chip filter outlined v-for="(item, i) in uniqueTags" :key="i">{{item}}</v-chip>
+        </v-chip-group>
+      </v-row>-->
+
       <v-row align="center" justify="center">
         <v-col
-          v-for="({ name, repoUrl, appUrl, description, screenshot },
+          v-for="({ name, repoUrl, appUrl, description, screenshot, tags, favorite},
           i) in projects"
           :key="i"
           cols="12"
           md="4"
         >
           <v-card>
-            <v-card-title>{{ name }}</v-card-title>
-
             <v-img :src="getImage(screenshot)" alt="project screenshot"></v-img>
 
-            <v-card-text>{{ description }}</v-card-text>
+            <v-card-title>
+              <span>{{ name }}</span>
+              <v-spacer />
+              <v-chip v-if="favorite" color="accent">
+                Favorite
+                <v-icon right>mdi-heart</v-icon>
+              </v-chip>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container fluid>
+                <v-row>{{description}}</v-row>
+                <v-row class="mt-4">
+                  <v-chip color="info" class="mr-3" v-for="(name, i) in tags" :key="i">{{name}}</v-chip>
+                </v-row>
+              </v-container>
+            </v-card-text>
 
             <v-card-actions>
-              <v-btn :href="appUrl" target="_blank" color="success">
-                Demo
-                <v-icon right>mdi-play-circle</v-icon>
-              </v-btn>
+              <v-btn :href="appUrl" target="_blank" color="success">See Demo</v-btn>
               <v-spacer></v-spacer>
               <v-btn icon :href="repoUrl" target="_blank">
-                <v-icon>mdi-github</v-icon>
+                <v-icon large>mdi-github</v-icon>
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -55,12 +70,21 @@ import data from "../store/projects.json";
 
 export default {
   data: () => ({
-    projects: data.projects,
+    projects: data.projects
   }),
+  computed: {
+    // I was thinking about allowing user to filter tags using tag group, but it seems to take more time to do this than I initially thought
+    // uniqueTags() {
+    //   let allTags = [];
+    //   this.projects.forEach(project => allTags.push(...project.tags));
+    //   let result = new Set(allTags);
+    //   return [...result];
+    // }
+  },
   methods: {
     getImage(fileName) {
       return require(`../assets/${fileName}`);
-    },
-  },
+    }
+  }
 };
 </script>
